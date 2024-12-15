7d5be6ab8c4d75b0feadce086dd4feed793d41ea
OnlyBitUseless <daniel.hei@proton.me>
2024-12-15 11:24:20 +0200

    Hello World! Vercel voidaan korjata myöhemmin. Pyöritä localhostina.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,19 @@
+// Serverin pohjusta
+
+// Tarvitut moduulit
 const express = require('express');
+const path = require('path');
+
+// Express sovellus
 const app = express();
 
+// Määritetään portti, jossa serveri pyörii
 const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    // Lähetetään sivun pohjana toimiva html tiedosto
+    res.sendFile(path.join(__dirname, '../frontend/html/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Running on Port ${PORT}`)
