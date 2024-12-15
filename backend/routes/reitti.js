4415976df3b07839eead3a9d17b18ceb25147cef
OnlyBitUseless <daniel.hei@proton.me>
2024-12-15 12:37:36 +0200

    reitti.js muokkauksia

--- backend/routes/reitti.js
+++ backend/routes/reitti.js
@@ -2,12 +2,22 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.sendStatus(404);
+router.get('/:aloitussana/:lopetussana', (req, res) => {
+    syöte = req.params;
+    console.log(syöte.aloitussana, syöte.lopetussana)
+
+    // Tarkistetaan, että syöte vastaa toivottua
+    if (String(syöte.aloitussana).length !== String(syöte.lopetussana).length) {
+        res.status(404).send({reitti: null, msg: 'sanat eivät voi olla eri pituisia'})
+    }
+    if (String(syöte.aloitussana) == String(syöte.lopetussana)) {
+        res.status(404).send({reitti: null, msg: 'sama sana'})
+    }
+    
+    // Tähän se reitinlöytöalgoritmi
+    res.send('success')
+
 })
 
-router.get('/:aloitussana/:lopetussana', (req, res) => {
-    
-})
 
 module.exports = router;
