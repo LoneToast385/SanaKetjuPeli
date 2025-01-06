1208db16b351bb995de21b94cd01043da4c6f7de
Benjamin Aalto <79770876+LoneToast385@users.noreply.github.com>
2025-01-06 17:17:18 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,8 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
     // Lähetetään sivun pohjana toimiva html tiedosto
     res.sendFile(path.join(__dirname, '../public/html/index.html'));
+    res.sendFile(path.join(__dirname, '../public/js/script.js'));
+    res.sendFile(path.join(__dirname, '../public/css/style.css'));
 });
 
 app.listen(PORT, () => {
