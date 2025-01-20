59ce3ea5818e1c9d4c8c40f96b1fe4ec75379639
OnlyBitUseless <daniel.hei@proton.me>
2025-01-20 19:57:55 +0200

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -353,7 +353,7 @@ span.onclick = function() {
 
 
 document.getElementById("tarkista-btn").addEventListener("click", checkWords);
-document.getElementById("tarkista-btn").addEventListener("click", clearBoxes);
+document.getElementById("puhdista-btn").addEventListener("click", clearBoxes);
 document.getElementById("uusi-btn").addEventListener("click", async () => {
   await loadWords();
   clearBoxes();
